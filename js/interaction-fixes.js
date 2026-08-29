ce4c82f0c618d573488fa305810c756e3c823cdb
greejiu <monggreee@naver.com>
2026-08-29 10:01:18 +0900

    chore: refresh grouped goal quick add

--- js/interaction-fixes.js
+++ js/interaction-fixes.js
@@ -12,7 +12,7 @@ import "./habit-start-date-fix.js?v=3";
 import "./habit-period-direct-save.js?v=1";
 import "./home-timeline-dynamic-columns.js?v=2";
 import "./goal-board-v2.js?v=3";
-import "./goal-quick-add.js?v=2";
+import "./goal-quick-add.js?v=3";
 import "./goal-group-board.js?v=1";
 import "./goal-status-drag.js?v=2";
 
